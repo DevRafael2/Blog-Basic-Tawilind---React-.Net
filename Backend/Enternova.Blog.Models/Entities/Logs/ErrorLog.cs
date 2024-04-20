f5e9e5db36f2cb9e90f95aea35da063c46a3b6b0
RafaelC123 <92170581+RafaelC123@users.noreply.github.com>
2024-04-20 15:18:22 -0500

    Se suben modelos de logs

---
+++ Backend/Enternova.Blog.Models/Entities/Logs/ErrorLog.cs
@@ -0,0 +1,28 @@
+﻿using Enternova.Blog.Models.Base;
+
+namespace Enternova.Blog.Models.Entities.Logs
+{
+    public class ErrorLog : BaseModel<Guid>
+    {
+        /// <summary>
+        /// Origen de la excepcion
+        /// </summary>
+        public string Source { get; set; }
+        /// <summary>
+        /// Mensaje de la excepción
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// Excepcion interna
+        /// </summary>
+        public string InnerException { get; set; }
+        /// <summary>
+        /// Traza de la operacion
+        /// </summary>
+        public string Trace { get; set; }
+        /// <summary>
+        /// Metodo de destino
+        /// </summary>
+        public string TargetSite { get; set; }
+    }
+}
---
+++ Backend/Enternova.Blog.Models/Entities/Logs/LoginLog.cs
@@ -0,0 +1,33 @@
+﻿using Enternova.Blog.Models.Base;
+using Enternova.Blog.Models.Entities.Security;
+
+namespace Enternova.Blog.Models.Entities.Logs
+{
+    public class LoginLog : BaseModel<Guid>
+    {
+        /// <summary>
+        /// Ip del dispositivo en donde se inicio sesión
+        /// </summary>
+        public string IpAdress { get; set; }
+        /// <summary>
+        /// Indica si el inicio de sesión fue exitoso
+        /// </summary>
+        public bool SignInSuccess { get; set; }
+        /// <summary>
+        /// Nombre del dispositivo en donde se inicio sesión
+        /// </summary>
+        public string DeviceName { get; set; }
+        /// <summary>
+        /// Sistema operativo del dispositivo donde se inició sesión
+        /// </summary>
+        public string OperatingSystem { get; set; }
+        /// <summary>
+        /// Id del usuario que logró/intentó iniciar sesión
+        /// </summary>
+        public long UserId { get; set; }
+        /// <summary>
+        /// Usuario que logró/intentó iniciar sesión
+        /// </summary>
+        public virtual User User { get; set; }
+    }
+}
