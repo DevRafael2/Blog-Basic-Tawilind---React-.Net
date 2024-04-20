cd95831bbf4f37c6340e5619f250c425afdf3af9
RafaelC123 <92170581+RafaelC123@users.noreply.github.com>
2024-04-20 15:39:20 -0500

    Dtos faltantes, se omitieron por el gitignore puesto que este omite las carpetas logs

---
+++ Backend/Enternova.Blog.Dtos/Entities/Logs/InUserLogin.cs
@@ -0,0 +1,29 @@
+﻿namespace Enternova.Blog.Dtos.Entities.Logs
+{
+    public class InUserLogin
+    {
+        /// <summary>
+        /// Nombre de usuario, en este caso email
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Contraseña
+        /// </summary>
+        public string Password { get; set; }
+
+
+
+        /// <summary>
+        /// Ip del origen / cliente
+        /// </summary>
+        public string OriginIp { get; set; }
+        /// <summary>
+        /// Nombre del dispositivo
+        /// </summary>
+        public string DeviceName { get; set; }
+        /// <summary>
+        /// Sistema operativo
+        /// </summary>
+        public string OperatingSystem { get; set; }
+    }
+}
---
+++ Backend/Enternova.Blog.Dtos/Entities/Logs/OutUserLogin.cs
@@ -0,0 +1,27 @@
+﻿namespace Enternova.Blog.Dtos.Entities.Logs
+{
+    public class OutUserLogin
+    {
+        /// <summary>
+        /// Nombre de usuario/email
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Token
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// Expiración
+        /// </summary>
+        public string Expire { get; set; }
+        /// <summary>
+        /// Nombre y apellido
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Id del usuario
+        /// </summary>
+        public long UserId { get; set; }
+
+    }
+}
